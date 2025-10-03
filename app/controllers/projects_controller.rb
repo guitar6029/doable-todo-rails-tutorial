4afcc99765d20cc2a5ef3d6ff50bb521b4b75235
Josh <jsdev305@gmail.com>
2025-10-03 14:46:31 -0400

    added show view for project/id added show to project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,10 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+    # projects/1
+    # params = { "id" => "1"}
+    @project = Project.find(params[:id])
+  end
 end
